6effe984ab781596f5da4026912a9d037e24767b
Rui <86rui.mendes@gmail.com>
2022-05-02 08:14:31 +0100

    computer choice, user choice and check winner functons done and tested

--- main.js
+++ main.js
@@ -1,78 +1,26 @@
 const choices = ['rock', 'paper', 'scissors'];
-const winners = [];
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        singleRound()
-    }
-    findGameWinner()
-}
-
-
-function singleRound() {
-    const playerSelection = userPlay();
-    const computerSelection = computerPlay();
-    const winner = checkWinner(playerSelection, computerSelection);
-    logPlays(playerSelection, computerSelection);
-
-    console.log(`winner: ${checkWinner(playerSelection, computerSelection)}`);
-    console.log('--------------')
-    winners.push(winner)
-}
-
 
 function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)];
-
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
 function userPlay() {
-    let input = prompt('please type Rock, Paper or Scissors');
+    let input = prompt('Please type Rock, Paper or Scissors')
     input = input.toLowerCase();
     return input;
 }
 
-function checkWinner(choiceP, choiceC) {
-
-
-    if (choiceP === choiceC) {
-        return 'tie';
+function checkWinner(computerC, userC) {
+    if (computerC == userC) {
+        return 'tie'
     } else if (
-        (choiceP === 'rock' && choiceC === 'scissors') ||
-        (choiceP === 'paper' && choiceC === 'rock') ||
-        (choiceP === 'scissors' && choiceC === 'paper')
+        (userC === 'rock' && computerC === 'scissors') ||
+        (userC === 'paper' && computerC === 'rock') ||
+        (userC === 'scissors' && computerC === 'paper')
     ) {
-        return 'Player';
+        return 'user'
+
     } else {
-        return 'Computer';
+        return 'computer'
     }
 }
-
-function logPlays(choiceP, choiceC) {
-    console.log(`You played ${choiceP}`);
-    console.log(`Computer played ${choiceC}`)
-}
-
-function findGameWinner() {
-    let playerCounter = 0;
-    let computerCounter = 0;
-
-    winners.forEach(element => {
-        if (element === 'Player') {
-            playerCounter += 1;
-        }
-        if (element == 'Computer') {
-            computerCounter += 1
-        }
-    });
-    if (playerCounter > computerCounter) {
-        console.log(`You win`);
-    } else if (playerCounter < computerCounter) {
-        console.log('computer wins')
-    } else {
-        console('it/s a tie')
-    }
-    console.log(`computer won ${computerCounter} times. You won ${playerCounter} times`)
-}
-
-game();
